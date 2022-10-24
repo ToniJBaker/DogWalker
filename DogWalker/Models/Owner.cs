0140b23633848905419dcc37579a8be10135fa39
Toni Baker <tonijbaker5@gmail.com>
2022-10-24 09:23:11 -0400

    dogwalker

---
+++ DogWalker/Models/Owner.cs
@@ -0,0 +1,15 @@
+﻿namespace DogWalker.Models
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public string Name { get; set; }
+        public string Address { get; set; }
+
+        public int NeighborhoodId { get; set; }
+        public string Phone { get; set; }
+        public Neighborhood Neighborhood { get; set; }
+    }
+}
---
+++ DogWalker/Repositories/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+﻿using DogWalker.Models;
+using Microsoft.Data.SqlClient;
+using System.Collections.Generic;
+
+namespace DogWalker.Repositories
+{
+    public interface IOwnerRepository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+    }
+}
